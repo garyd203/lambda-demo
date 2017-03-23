58b58b4c4092802c49e3595ba0d3df09850bf35a
Gary Donovan <gazza@gazza.id.au>
2017-03-23 12:52:31 +1100

    New function to get tallies directly

--- handler.py
+++ handler.py
@@ -1,22 +1,37 @@
+from garys_auth import get_garys_twitter_auth
+from twitter import *
 import json
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+def harvest(event, context):
+    t = Twitter(auth=get_garys_twitter_auth())
 
-    return response
+    #TODO or refactor to perform the request dynamically, with a cache on dynamo?
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    """
+    # NB: From https://dev.twitter.com/rest/public/search...
+    # "The Search API is not complete index of all Tweets, but instead an
+    #  index of recent Tweets. The index includes between 6-9 days of
+    #  Tweets".
+    #
+    # If you care, use the streaming API instead :-)
+    response = t.search.tweets(q='#sypy', result_type='recent', since_id=0)
+    for tweet in response['statuses']:
+        print tweet['text']
+        print tweet['user']['screen_name']
+        # Save in Dynamo
+    # Store max_id in dynamo as new since_id
+
+    return "Go SyPy"
+
+
+def user_tally(event, context):
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Go SyPy"
+        }),
+        }
+
+
+if __name__ == '__main__':
+    user_tally({}, {})
